c14a0b935f5569e4a7740da0c2afb634726653b1
Jayesh saindane <126108590+Jayeshsaindane@users.noreply.github.com>
2023-02-23 00:29:29 +0530

    Update averageofarray.cpp

--- averageofarray.cpp
+++ averageofarray.cpp
@@ -4,25 +4,3 @@ using namespace std;
     
 int main()
 {
-    int sum=0,avg;
-    int n;
-    cout<<"Enter Number of elements in Array:";
-    cin>>n;
-    int A[n];
-    cout<<"Enter elements in Array:";
-    for(int i=0;i<n;i++){
-        cin>>A[i];
-    }
-    for(auto x:A){
-        cout<<"Array is:";
-        cout<<"["<<x<<"]"<<endl;
-    }
-    for(int i=0;i<n;i++){
-        sum=sum + A[i];
-    }
-    avg=sum/n;
-    cout<<"the Average of all elements in given Array is:"<<avg;
-
-    
-    return 0;
-}  
