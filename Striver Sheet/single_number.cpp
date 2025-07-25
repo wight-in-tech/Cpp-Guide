c7c57f1bb9378cc68a58a1786b71179a93651739
wight-in-tech <chetannb78@gmail.com>
2025-07-25 22:22:02 +0530

    Update single_number.cpp

--- Striver Sheet/single_number.cpp
+++ Striver Sheet/single_number.cpp
@@ -1,3 +1,15 @@
+//Link: https://leetcode.com/problems/single-number/description/
+
+
+
+//Description: 136. Single Number
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+
+
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
