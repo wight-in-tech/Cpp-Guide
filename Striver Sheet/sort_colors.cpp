7a3a2b7c19ecd51baec3207c6a5199b436b6a129
wight-in-tech <chetannb78@gmail.com>
2025-07-27 21:11:38 +0530

    Update sort_colors.cpp

---
+++ Striver Sheet/sort_colors.cpp
@@ -0,0 +1,19 @@
+
+
+//link:https://leetcode.com/problems/sort-colors/
+
+//description:75. Sort Colors
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+        
+    }
+};
